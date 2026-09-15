e586db5f5408f1f6eaba263341fe4ee9019c569d
camilo-axm <camilo.aguirre-g@mail.escuelaing.edu.co>
2026-09-14 22:19:47 -0500

    refactor: complemento de pruebas para validacion de drones nulos, vacios y duplicados

--- skyrescue-tdd/src/test/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenterTest.java
+++ skyrescue-tdd/src/test/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenterTest.java
@@ -18,4 +18,39 @@ class RescueCenterTest {
         // Assert
         assertTrue(result);
     }
+    @Test
+    void shouldReturnFalseWhenDroneIsNull() {
+        // Act
+        boolean result = center.addDrone(null);
+
+        // Assert
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldReturnFalseWhenDroneIdIsEmpty() {
+        // Arrange
+        Drone drone = new Drone("", "Rescue-X", 50);
+
+        // Act
+        boolean result = center.addDrone(drone);
+
+        // Assert
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldReturnFalseWhenDroneIdAlreadyExists() {
+        // Arrange
+        Drone drone1 = new Drone("D-101", "Rescue-X", 50);
+        Drone drone2 = new Drone("D-101", "Rescue-Y", 80);
+        center.addDrone(drone1);
+
+        // Act
+        boolean result = center.addDrone(drone2);
+
+        // Assert
+        assertFalse(result);
+    }
+
 }
